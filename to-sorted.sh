1a4b3cf1a09f6ce94878eae762f98cfff787f807
hugolpz <hugo.lpz@gmail.com>
2021-03-03 17:37:18 +0100

    add helper scripts

---
+++ to-sorted.sh
@@ -0,0 +1,7 @@
+mkdir -p ./frequency-sorted-hash ./frequency-sorted-count
+for filename in ./unilex/data/frequency/*.txt; do
+    [ -e "$filename" ] || continue
+    echo "Processing: $(basename "$filename" .txt).txt "
+    cat ./unilex/data/frequency/$(basename "$filename" .txt).txt | tail -n +5 | sort -k 2,2 -n -r | cut -d$'\t' -f1 | sed -E 's/^/# /g' > ./frequency-sorted-hash/$(basename "$filename" .txt).txt
+    cat ./unilex/data/frequency/$(basename "$filename" .txt).txt | tail -n +5 | sort -k 2,2 -n -r > ./frequency-sorted-count/$(basename "$filename" .txt).txt
+done
